48a657ef02718865a5c8da9cef038fbad8525d2a
Yufei QIU <yufei.qiu@schneider-electric.com>
2019-12-30 16:59:30 +0800

    Stash
    Change-Id: I293c1c5072553c559dc4545fff23cb62f6947654

--- Analyzer/dummy_plugin_for_test.py
+++ Analyzer/dummy_plugin_for_test.py
@@ -26,9 +26,24 @@ finally:
 
 def plugin_prob() -> dict:
     return {
+        'plugin_id': '2806f676-2aad-11ea-8c57-87a2a5d9cf76',
         'plugin_name': 'dummy_plugin_for_test',
         'plugin_version': '0.0.0.1',
-        'tags': ['test', 'dummy', 'analyzer']
+        'tags': ['test', 'dummy', 'analyzer'],
+        'manual': {
+            'score': [
+                ('5d19927a-2ab1-11ea-aee4-eb8a702e7495', '评分测试1', '传入的代码末位是几，就打几分。'),
+                ('bc74b6fa-2ab1-11ea-8b94-03e35eea3ca4', '评分测试2', '传入的代码末位是几，就打10-几分。'),
+            ],
+            'inclusive': [
+                ('6b23435c-2ab1-11ea-99a8-3f957097f4c9', '包含测试1', '只有代码末位是1-5的能通过此测试。'),
+                ('d0b619ba-2ab1-11ea-ac32-43e650aafd4f', '包含测试2', '只有代码末位是5-9的能通过此测试。'),
+            ],
+            'exclusive': [
+                ('78ffae34-2ab1-11ea-88ff-634c407b44d3', '排除测试1', '只有代码第一位是1-5的会被排除'),
+                ('d905cdea-2ab1-11ea-9e79-ff65d4808d88', '排除测试2', '只有代码第一位是5-9的会被排除'),
+            ],
+        }
     }
 
 
@@ -54,15 +69,15 @@ def manual() -> dict:
     }
 
 
-def score(securities: [str], data_hub: DataHubEntry) -> [AnalysisResult]:
+def score(securities: [str], methods: [str], data_hub: DataHubEntry) -> [AnalysisResult]:
     pass
 
 
-def inclusive(securities: [str], data_hub: DataHubEntry) -> ([str], [AnalysisResult]):
+def inclusive(securities: [str], methods: [str], data_hub: DataHubEntry) -> ([str], [AnalysisResult]):
     pass
 
 
-def exclusive(securities: [str], data_hub: DataHubEntry) -> ([str], [AnalysisResult]):
+def exclusive(securities: [str], methods: [str], data_hub: DataHubEntry) -> ([str], [AnalysisResult]):
     pass
 
 
